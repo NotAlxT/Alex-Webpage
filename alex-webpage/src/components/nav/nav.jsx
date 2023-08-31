51b65cf04584f95b633bb9a9738037e24960f0c8
NotAlxT <anhntran56@gmail.com>
2023-08-31 17:37:25 -0400

    c
    c

--- alex-webpage/src/components/nav/nav.jsx
+++ alex-webpage/src/components/nav/nav.jsx
@@ -3,6 +3,7 @@ import './nav.css'
 // import Logo from '../../imgs/logo-white.png'
 import { Icon } from '@iconify/react';
 import Loader from '../loader/loader';
+import { Link } from 'react-router-dom';
 
 export default function Nav() {
     return (
@@ -10,9 +11,9 @@ export default function Nav() {
               {/* <img src={Logo} alt="Logo" />   */}
               <Loader />
               <div className='navLinks'>
-                <a href='/'><Icon icon="solar:home-2-broken" width="40" height="40" /></a>
-                <a href='/profile'><Icon icon="fluent-mdl2:contact" width="40" height="40" /></a>
-                <a href='/portfolio'><Icon icon="solar:folder-with-files-broken" width="40" height="40" /></a>
+                <Link to='/'><Icon icon="solar:home-2-broken" width="40" height="40" /></Link>
+                <Link to='/profile'><Icon icon="fluent-mdl2:contact" width="40" height="40" /></Link>
+                <Link to='/portfolio'><Icon icon="solar:folder-with-files-broken" width="40" height="40" /></Link>
               </div>
         </div>
     )
