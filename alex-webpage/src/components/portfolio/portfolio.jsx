323e0896fb400bd0639390e9c1becdfaddf8b75f
NotAlxT <anhntran56@gmail.com>
2023-09-06 20:14:09 -0400

    Portfolio page
    created layout

--- alex-webpage/src/components/portfolio/portfolio.jsx
+++ alex-webpage/src/components/portfolio/portfolio.jsx
@@ -1,7 +1,133 @@
 import React from 'react'
+import './portfolio.css'
 
 export default function Portfolio() {
   return (
-    <div>portfolio</div>
+    <div>
+
+
+      <div className="pSection1">
+        <section className='pLeft'>
+          <div className="portfolioCard">
+            <div>
+              image here
+            </div>
+            <div className="pCardContent">
+              <h1>Title</h1>
+              <p>Languages used here</p>
+            </div>
+          </div>
+        </section>
+        <section className='pRight'>
+          <div>
+            <p>Info here</p>
+          </div>
+        </section>
+      </div>
+
+
+      <div className="pSection2">
+        <section className='pLeft'>
+          <div>
+            <p>Info here</p>
+          </div>
+        </section>
+        <section className='pRight'>
+          <div className="portfolioCard2">
+            <div>
+              image here
+            </div>
+            <div className="pCardContent2">
+              <h1>Title</h1>
+              <p>Languages used here</p>
+            </div>
+          </div>
+
+        </section>
+      </div>
+
+      <div className="pSection1">
+        <section className='pLeft'>
+          <div className="portfolioCard">
+            <div>
+              image here
+            </div>
+            <div className="pCardContent">
+              <h1>Title</h1>
+              <p>Languages used here</p>
+            </div>
+          </div>
+        </section>
+        <section className='pRight'>
+          <div>
+            <p>Info here</p>
+          </div>
+        </section>
+      </div>
+
+
+      <div className="pSection2">
+        <section className='pLeft'>
+          <div>
+            <p>Info here</p>
+          </div>
+        </section>
+        <section className='pRight'>
+          <div className="portfolioCard2">
+            <div>
+              image here
+            </div>
+            <div className="pCardContent2">
+              <h1>Title</h1>
+              <p>Languages used here</p>
+            </div>
+          </div>
+
+        </section>
+      </div>
+
+      <div className="pSection1">
+        <section className='pLeft'>
+          <div className="portfolioCard">
+            <div>
+              image here
+            </div>
+            <div className="pCardContent">
+              <h1>Title</h1>
+              <p>Languages used here</p>
+            </div>
+          </div>
+        </section>
+        <section className='pRight'>
+          <div>
+            <p>Info here</p>
+          </div>
+        </section>
+      </div>
+
+
+      <div className="pSection2">
+        <section className='pLeft'>
+          <div>
+            <p>Info here</p>
+          </div>
+        </section>
+        <section className='pRight'>
+          <div className="portfolioCard2">
+            <div>
+              image here
+            </div>
+            <div className="pCardContent2">
+              <h1>Title</h1>
+              <p>Languages used here</p>
+            </div>
+          </div>
+
+        </section>
+      </div>
+
+
+
+    </div>
   )
 }
