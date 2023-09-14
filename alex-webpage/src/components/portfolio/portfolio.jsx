a782b2cfd885f323ecc622dfb3b778bdb3d1dc23
NotAlxT <anhntran56@gmail.com>
2023-09-14 10:08:58 -0400

    c
    c

--- alex-webpage/src/components/portfolio/portfolio.jsx
+++ alex-webpage/src/components/portfolio/portfolio.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './portfolio.css'
 
 export default function Portfolio() {
+  const myRef = useRef()
+  const [elementVisible, setElementVisible] = useState()
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const entry = entries[0];
+      setElementVisible(entry.isIntersecting)
+      console.log('elementVisible', elementVisible)
+    })
+    observer.observe(myRef.current)
+
+  }, [elementVisible])
+
   return (
     <div>
 
@@ -9,7 +21,47 @@ export default function Portfolio() {
       <div className="pSection1">
         <section className='pLeft'>
           <div className="portfolioCard">
-            <div>
+            <div ref={myRef}>
+              image here
+            </div>
+            <div className="pCardContent">
+              <h1>Title</h1>
+              <p>Languages used here</p>
+            </div>
+          </div>
+        </section>
+        <section className='pRight'>
+          <div>
+            <p>Info here</p>
+          </div>
+        </section>
+      </div>
+
+
+      <div className="pSection2">
+        <section className='pLeft'>
+          <div>
+            <p>Info here</p>
+          </div>
+        </section>
+        <section className='pRight'>
+          <div className="portfolioCard2">
+            <div ref={myRef}>
+              image here
+            </div>
+            <div className="pCardContent2">
+              <h1>Title</h1>
+              <p>Languages used here</p>
+            </div>
+          </div>
+
+        </section>
+      </div>
+
+      <div className="pSection1">
+        <section className='pLeft'>
+          <div className="portfolioCard">
+            <div ref={myRef}>
               image here
             </div>
             <div className="pCardContent">
@@ -84,47 +136,7 @@ export default function Portfolio() {
           </div>
 
         </section>
-      </div>
-
-      <div className="pSection1">
-        <section className='pLeft'>
-          <div className="portfolioCard">
-            <div>
-              image here
-            </div>
-            <div className="pCardContent">
-              <h1>Title</h1>
-              <p>Languages used here</p>
-            </div>
-          </div>
-        </section>
-        <section className='pRight'>
-          <div>
-            <p>Info here</p>
-          </div>
-        </section>
-      </div>
-
-
-      <div className="pSection2">
-        <section className='pLeft'>
-          <div>
-            <p>Info here</p>
-          </div>
-        </section>
-        <section className='pRight'>
-          <div className="portfolioCard2">
-            <div>
-              image here
-            </div>
-            <div className="pCardContent2">
-              <h1>Title</h1>
-              <p>Languages used here</p>
-            </div>
-          </div>
-
-        </section>
-      </div>
+      </div> 
 
 
 
