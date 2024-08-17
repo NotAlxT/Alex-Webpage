ddd81a8479a09f75a5f896b198e324a73fb3f116
Alex (Anh) Tran <anhntran56@gmail.com>
2024-08-16 20:26:10 -0400

    commit profiles page updates
    added another section of content along with updating the first content sections

--- alex-webpage/src/components/profile/profile.jsx
+++ alex-webpage/src/components/profile/profile.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Nav from "../nav/nav";
 import "./profile.css";
-import CodingBG from "../../imgs/Coding2.jpg"
+import CodingBG from "../../imgs/Coding2.jpg";
+import House from "../../imgs/House.jpg";
 
 export default function Profile() {
   return (
-    <div className="w-full h-full bg-contain bg-black ">
-      <div className="flex flex-col w-full justify-center items-center gap-10">
+    <div className="flex justify-center items-center w-full h-full bg-black ">
+      
+      <div className="flex flex-col w-[95%] justify-center items-center gap-10">
         {/*naviation */}
         <Nav />
+
         <div className="flex justify-center items-center w-full h-[70vh] flex-col gap-5">
           <div className="text-white text-9xl ">Profile</div>
           {/*Picture animator*/}
@@ -21,13 +24,16 @@ export default function Profile() {
         {/*Line divider horizontal*/}
         <div className="bg-[rgba(159,159,159,0.5)] w-[70%] h-[1px] rounded-lg mt-10 "></div>
         {/*Content */}
-        <div className="bg-[#0b0b0b] w-full h-full flex flex-row justify-center items-center">
+        <div className=" bg-gradient-to-r from-black via-[#121212] to-[#202020] w-full h-full flex flex-row justify-center items-center gap-5 p-5 rounded">
           {/* Left Content*/}
           <div className="flex w-[60%]">
-            <div> 
-              <img className="object-cover h-full w-full" src={CodingBG} alt="Coding Background" />
-          </div>
-           
+            <div>
+              <img
+                className="object-cover h-full w-full rounded"
+                src={CodingBG}
+                alt="Coding Background"
+              />
+            </div>
           </div>
           {/*Divider Verticle*/}
           <div className="flex h-full w-[.5px] bg-[rgb(58,58,58)]"> </div>
@@ -42,14 +48,14 @@ export default function Profile() {
                 </h1>
               </div>
 
-              {/* <div className="flex w-[70%] h-[.5px] bg-[rgb(58,58,58)]"> </div> */}
-
-              <div className="flex text-xl justify-start text-white">
+              <div className="flex text-2xl justify-start text-white">
                 <h1>Skills/Tech</h1>
               </div>
 
+              {/*Line divider horizontal*/}
               <div className="flex w-[70%] h-[.5px] bg-[rgb(58,58,58)]"> </div>
 
+              {/* Skill Carousel Upper */}
               <div className="flex justify-center w-[70%] overflow-hidden text-black">
                 <div className="flex justify-center w-full text-[rgb(83,83,83)] gap-10 slide">
                   <p>HTML</p>
@@ -78,6 +84,7 @@ export default function Profile() {
                 </div>
               </div>
 
+              {/* Skill Carousel Lower */}
               <div className="flex justify-center w-[70%] overflow-hidden text-black ">
                 <div className="flex justify-center w-full text-[rgb(83,83,83)] gap-10 slideReverse">
                   <p>Python</p>
@@ -101,13 +108,16 @@ export default function Profile() {
                   <p>Node.js</p>
                 </div>
               </div>
+
+              {/*Line divider horizontal*/}
               <div className="flex w-[70%] h-[.5px] bg-[rgb(58,58,58)]"> </div>
 
               <div className="flex flex-col w-[70%] gap-5">
-                <div className="flex justify-center text-xl text-white">
+                <div className="flex justify-center text-2xl text-white">
                   Education
                 </div>
 
+                {/*Line divider horizontal*/}
                 <div className="flex w-full h-[.5px] bg-[rgb(58,58,58)]"> </div>
 
                 <div className="flex flex-col ">
@@ -131,25 +141,120 @@ export default function Profile() {
               </div>
 
               <div className="flex flex-col w-[70%] gap-5">
-                <div className="flex justify-center text-xl text-white">
+                <div className="flex justify-center text-2xl text-white">
                   Experiance
                 </div>
 
+                {/*Line divider horizontal*/}
                 <div className="flex w-full h-[.5px] bg-[rgb(58,58,58)]"> </div>
 
                 <div className="flex flex-col ">
                   <div className="flex text-xl text-white">CourtCheck</div>
                   <div className="flex text-[rgb(83,83,83)] justify-between">
-                   <p>Front-End Engineer</p>
-                   <p>Dec, 2023 - Present</p>
+                    <p>Front-End Engineer</p>
+                    <p>Dec, 2023 - Present</p>
                   </div>
                 </div>
               </div>
 
+              {/*Line divider horizontal*/}
               <div className="flex w-[70%] h-[.5px] bg-[rgb(58,58,58)]"> </div>
-
             </div>
           </div>
+        </div>
+
+        {/*Content #2 */}
+        <div className="bg-gradient-to-r from-white via-gray-500 to-black w-full h-full flex flex-row justify-center items-center gap-5 p-5 rounded">
+
+          {/* Left Content */}
+          <div className="flex flex-col justify-center w-[40%] gap-5">
+            <div className="flex flex-col w-full items-center gap-5">
+              <div className="flex flex-col w-[70%]">
+                <h1 className="text-black text-4xl">Real Estate Developer</h1>
+                <h1 className="flex text-[rgb(45,45,45)] text-3xl">
+                  About Me
+                </h1>
+              </div>
+
+              <div className="flex text-2xl justify-start text-black">
+                <h1>Properties</h1>
+              </div>
+
+              {/*Line divider horizontal*/}
+              <div className="flex w-[70%] h-[.5px] bg-[rgb(58,58,58)]"> </div>
+              <div className="flex flex-col text-[rgb(83,83,83)]">
+                <p>Bridgewater, MA </p>
+                <br />
+                <p>Port Charlotte, FL</p>
+              </div>
+             
+              {/*Line divider horizontal*/}
+              <div className="flex w-[70%] h-[.5px] bg-[rgb(58,58,58)]"> </div>
+
+              <div className="flex flex-col w-[70%] gap-5">
+                <div className="flex justify-center text-2xl text-black">
+                  Education
+                </div>
+
+                {/*Line divider horizontal*/}
+                <div className="flex w-full h-[.5px] bg-[rgb(58,58,58)]"> </div>
+
+                <div className="flex flex-col ">
+                  <div className="flex text-xl text-black">Host Group Real Estate Academy</div>
+                  <div className="flex text-[rgb(83,83,83)]">
+                    Real Estate Salesman 
+                  </div>
+                </div>
+                <div className="flex flex-col ">
+                  <div className="flex text-xl  text-black">Keller Williams Realty</div>
+                  <div className="text-[rgb(83,83,83)]">
+                    Residential Realtor
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex flex-col w-[70%] gap-5">
+                <div className="flex justify-center text-2xl text-black">
+                  Experiance
+                </div>
+
+                {/*Line divider horizontal*/}
+                <div className="flex w-full h-[.5px] bg-[rgb(58,58,58)]"> </div>
+
+                <div className="flex flex-col ">
+                  <div className="flex text-xl text-black">Keller Williams Realty</div>
+                  <div className="flex text-[rgb(83,83,83)] justify-between">
+                    <p>Residential Realtor</p>
+                    <p>Aug, 2019 - Jan, 2024</p>
+                  </div>
+
+                  <div className="flex text-xl text-black">Property Management</div>
+                  <div className="flex text-[rgb(83,83,83)] justify-between">
+                    <p>LandLord </p>
+                    <p>Aug, 2023 - Present</p>
+                  </div>
+                </div>
+              </div>
+
+              {/*Line divider horizontal*/}
+              <div className="flex w-[70%] h-[.5px] bg-[rgb(58,58,58)]"> </div>
+            </div>
+          </div>
+
+          {/*Divider Verticle*/}
+          <div className="flex h-full w-[.5px] bg-[rgb(58,58,58)]"> </div>
+
+          {/* Right Content*/}
+          <div className="flex w-[60%]">
+            <div>
+              <img
+                className="object-cover h-full w-full rounded"
+                src={House}
+                alt="House Background"
+              />
+            </div>
+          </div>
+          
         </div>
       </div>
     </div>
